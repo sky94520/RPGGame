0b7eaceb8b5f029851aa7bd7d750281c964cda72
sky94520 <541052067@qq.com>
2019-10-17 11:01:48 +0800

    lua脚本文件

--- RPGGame/Resources/script/map01.lua
+++ RPGGame/Resources/script/map01.lua
@@ -2,16 +2,17 @@ require "rpg_core"
 --创建几个门
 object.create("door1", "Door1_0", 6, 7);
 
-object.setTrigger("door1", TriggerType.Click, "change2map01_01");
-object.setPriority("door1", Priority.Same);
+object.setTrigger("door1", TriggerType.Touch, "change2map01_01");
+object.setPriority("door1", Priority.Low);
 
 
 function change2map01_01( ... )
 	-- body
 	print("change to map01_01");
 	base.setGameState(GameState.Script);
-	timer.delay(1.5);
-	print("delay 1.5 second");
+	--fadeIn
+	screen.fadeIn(1.5);
+	screen.fadeOut(1.5);
 	base.setGameState(GameState.Normal);
 	print("script end");
 end
---
+++ RPGGame/Resources/script/rpg_core.lua
@@ -0,0 +1,29 @@
+--包含着一些枚举体和一些函数
+--游戏状态枚举体
+GameState = {};
+GameState.Normal = 0;
+GameState.Fighting = 1;
+GameState.Script = 2;
+
+--脚本触发方式
+TriggerType = {};
+TriggerType.Click = 1;
+TriggerType.Touch = 2;
+TriggerType.All = 3;
+
+--优先级
+Priority = {};
+Priority.Same = 0;
+Priority.Low = 1;
+Priority.Hight = 2;
+--方向
+Direction = {};
+Direction.Down = 0;
+Direction.Left = 1;
+Direction.Right = 2;
+Direction.Up = 3;
+--获取对应数组
+function Direction:toArray()
+	-- body
+	return {self.Down,self.Left,self.Right,self.Up};
+end
