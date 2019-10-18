74e501319edbd3a5f56239015b045bbdaa4df983
sky94520 <541052067@qq.com>
2019-10-18 21:35:47 +0800

    添加传送阵，目前脚本对象的获取出现一些问题

---
+++ RPGGame/Resources/script/map01_01.lua
@@ -0,0 +1,19 @@
+require "rpg_core"
+--创建一个门
+object.create("portal1", "Portal_1", 8, 13);
+
+object.setTrigger("portal1", TriggerType.Touch, "change2map01");
+object.setPriority("portal1", Priority.Low);
+
+
+function change2map01( ... )
+	-- body
+	print("change to map01");
+	base.setGameState(GameState.Script);
+	--fadeIn
+	screen.fadeIn(1.5);
+	--跳转地图
+	movement.changeMap("map/Map01.tmx", 6, 6);
+	screen.fadeOut(1.5);
+	base.setGameState(GameState.Normal);
+end
